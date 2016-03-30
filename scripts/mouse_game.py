c89f4c81f8ce53e26997034ea65470479cf4d9ec
Ndivhuwo <ndivhuwo9@gmail.com>
2016-03-30 10:12:24 +0900

    Running average

--- scripts/mouse_game.py
+++ scripts/mouse_game.py
@@ -5,9 +5,10 @@ import random
 import time
 
 target = 20
-num_play = 10**12
+num_play = 10**8
+sum_n = 0
 
-turns = [0]*num_play
+#turns = [0]*num_play
 #turns_n = []
 #turns_count = []
 player1 = Player(1)
@@ -17,7 +18,9 @@ startT = time.time()
 for i in range(num_play):
 	
 	game1.play(player1)
-	turns[i] = player1.turns
+	#turns[i] = player1.turns
+	sum_n += player1.turns
+	
 	"""
 	if(player1.turns in turns_n):
 		turns_count[turns_n.index(player1.turns)] += 1
@@ -29,18 +32,20 @@ for i in range(num_play):
 	if(i%10000 == 0):
 		endT = time.time()
 		print("Loop %d, time: %f seconds" % (i,endT-startT))
-		print("Player earned %d points in %d turns!" % (player1.points, player1.turns))
+		#print("Player earned %d points in %d turns!" % (player1.points, player1.turns))
+		print("Average turns: %f" % float(sum_n/(i+1)))
 	player1.reset()
 	game1.reset()
 
 endT = time.time()
 print("It took %f seconds" % (endT-startT))
-print("Expected number of turns is %f" % float(sum(turns)/num_play))
+#print("Expected number of turns is %f" % float(sum(turns)/num_play))
 """
 expN = 0
 for i in range(len(turns_n)):
 	expN += turns_count[i]*turns_n[i]
 expN = expN/num_play
-
-print("Expected number of turns is %f" % expN)
 """
+print("Expected number of turns is %f" % float(sum_n/num_play))
+#print("True expected number of turns is %f" % expN)
+
